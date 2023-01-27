72c74ff8d9c294490c26994a35838c16d0204ae4
Robert <61297366+RobertOcsV@users.noreply.github.com>
2023-01-27 13:16:43 -0300

    links de navegação dinâmicos

--- projetoAulaForms/src/app/navegacao/menu/menu.component.ts
+++ projetoAulaForms/src/app/navegacao/menu/menu.component.ts
@@ -4,4 +4,47 @@ import { Component } from '@angular/core';
   selector: 'app-menu',
   templateUrl: './menu.component.html'
 })
-export class MenuComponent {}
+export class MenuComponent {
+
+  
+nav: Nav[] = [
+
+  {
+    link: '/home',
+    name: 'Home',
+    exact: true,
+    admin: false
+  },
+  {
+    link: '/cadastro',
+    name: 'Cadastro',
+    exact: true,
+    admin: false
+  },
+  {
+    link: '/sobre',
+    name: 'Sobre',
+    exact: true,
+    admin: false
+  },
+  {
+    link: '/produtos',
+    name: 'Produtos',
+    exact: true,
+    admin: false
+  },
+];
+
+
+}
+
+
+
+//não recomendável, normalmente criaremos uma interfacec em outro arquivo, esta interface 
+//será apenas de exemplo para criação de links dinâmicos
+interface Nav{
+  link: string,
+  name: string,
+  exact: boolean,
+  admin: boolean
+}
