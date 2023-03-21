6fb592dabc8f98128a9be429ffffb66a39ae54c7
Robert <61297366+RobertOcsV@users.noreply.github.com>
2023-03-21 15:02:20 -0300

    template basic projetofinal

--- projetoFinalDevIo/front-end/src/app/navegacao/navegacao.module.ts
+++ projetoFinalDevIo/front-end/src/app/navegacao/navegacao.module.ts
@@ -1,14 +1,28 @@
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MenuComponent } from './menu/menu.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-    declarations:[],
+    declarations:[
+      MenuComponent,
+      NotFoundComponent,
+      HomeComponent,
+      FooterComponent
+    ],
     imports:[
       CommonModule,
       RouterModule
     ],
-    exports:[]
+    exports:[
+      MenuComponent,
+      NotFoundComponent,
+      HomeComponent,
+      FooterComponent
+    ]
 })
 
 export class NavegacaoModule { }
