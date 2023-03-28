93f1608423f594b289702aa1ba3fd45c7192ee73
Robert <61297366+RobertOcsV@users.noreply.github.com>
2023-03-28 01:39:05 -0300

    Cadastro de novo usuário

--- projetoFinalDevIo/front-end/src/app/conta/cadastro/cadastro.component.ts
+++ projetoFinalDevIo/front-end/src/app/conta/cadastro/cadastro.component.ts
@@ -1,22 +1,23 @@
-import { ValidationMessages, GenericValidator, DisplayMessage } from './../../utils/generic-form-validation';
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChildren } from '@angular/core';
-import { FormBuilder, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
-import { Usuario } from '../models/usuario';
-import { ContaService } from '../services/conta.service';
-import { CustomValidators } from 'ngx-custom-validators';
+import { Component, OnInit, AfterViewInit, ViewChildren, ElementRef } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl, FormControlName } from '@angular/forms';
+import { Router } from '@angular/router';
+
 import { Observable, fromEvent, merge } from 'rxjs';
 
+import { CustomValidators } from 'ngx-custom-validators';
 
+
+import { Usuario } from '../models/usuario';
+import { ContaService } from '../services/conta.service';
+import { ValidationMessages, GenericValidator, DisplayMessage } from 'src/app/utils/generic-form-validation';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html'
-
 })
 export class CadastroComponent implements OnInit, AfterViewInit {
 
   @ViewChildren(FormControlName, { read: ElementRef }) formInputElements: ElementRef[];
-
 
   errors: any[] = [];
   cadastroForm: FormGroup;
@@ -24,35 +25,37 @@ export class CadastroComponent implements OnInit, AfterViewInit {
 
   validationMessages: ValidationMessages;
   genericValidator: GenericValidator;
-  displayMessage: DisplayMessage;
+  displayMessage: DisplayMessage = {};
+
+  mudancasNaoSalvas: boolean;
 
   constructor(private fb: FormBuilder,
-              private contaService: ContaService)
-     {
-      this.validationMessages = {
-        email: {
-          required: 'Informe o e-mail',
-          email: 'Email inválido'
-        },
-        password: {
-          required: 'Informe a senha',
-          rangeLength: 'A senha deve possuir entre 6 e 15 caracteres'
-        },
-        confirmPassword: {
-          required: 'Informe a senha novamente',
-          rangeLength: 'A senha deve possuir entre 6 e 15 caracteres',
-          equalTo: 'As senhas não conferem'
-        }
-      };
+    private contaService: ContaService,
+    private router: Router) {
 
-      this.genericValidator = new GenericValidator(this.validationMessages);
+    this.validationMessages = {
+      email: {
+        required: 'Informe o e-mail',
+        email: 'Email inválido'
+      },
+      password: {
+        required: 'Informe a senha',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres'
+      },
+      confirmPassword: {
+        required: 'Informe a senha novamente',
+        rangeLength: 'A senha deve possuir entre 6 e 15 caracteres',
+        equalTo: 'As senhas não conferem'
+      }
+    };
 
-     }
+    this.genericValidator = new GenericValidator(this.validationMessages);
+  }
+
   ngOnInit(): void {
 
     let senha = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15])]);
     let senhaConfirm = new FormControl('', [Validators.required, CustomValidators.rangeLength([6, 15]), CustomValidators.equalTo(senha)]);
-
 
     this.cadastroForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -63,18 +66,25 @@ export class CadastroComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     let controlBlurs: Observable<any>[] = this.formInputElements
-      .map((formControl: ElementRef) =>fromEvent(formControl.nativeElement, 'blur'));
+      .map((formControl: ElementRef) => fromEvent(formControl.nativeElement, 'blur'));
 
-      merge(...controlBlurs).subscribe(()=> {
-        this.displayMessage = this.genericValidator.processarMensagens(this.cadastroForm);
-      });
+    merge(...controlBlurs).subscribe(() => {
+      this.displayMessage = this.genericValidator.processarMensagens(this.cadastroForm);
+      this.mudancasNaoSalvas = true;
+    });
   }
 
-  adicionarConta(){
-    if(this.cadastroForm.dirty && this.cadastroForm.valid){
-      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value );
+  adicionarConta() {
+    if (this.cadastroForm.dirty && this.cadastroForm.valid) {
+      this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
 
-      this.contaService.cadastrarUsuario(this.usuario);
+      this.mudancasNaoSalvas = false;
     }
   }
+
+  processarSucesso(response: any) {
+    this.cadastroForm.reset();
+    this.errors = [];
+  }
+
 }
